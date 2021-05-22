7bc27dda1fc654a637215b451e330ed67a288240
Physalis-1 <84348788+Physalis-1@users.noreply.github.com>
2021-05-22 13:58:19 +0300

    Update Compiller.py

--- PROGA/Compiller.py
+++ PROGA/Compiller.py
@@ -12,7 +12,7 @@ def run(llvm_ir):
     mod.verify()
 
     pmb = llvm.create_pass_manager_builder()
-    pmb.opt_level = 2
+    pmb.opt_level = 1
     pm = llvm.create_module_pass_manager()
     pmb.populate(pm)
     pm.run(mod)
@@ -24,3 +24,18 @@ def run(llvm_ir):
     main_ptr = engine.get_function_address('__init_main')
     main_func = ctypes.CFUNCTYPE(None)(main_ptr)
     main_func()
+
+if __name__ == '__main__':
+    import sys
+    import Parser
+    import Table
+    import TAC
+    import Compiller
+    import llvmgen
+    text = open(sys.argv[1]).read()
+    AST = Parser.start_parser(text)
+    table = Table.table_scope(AST)
+    tac = TAC.start(AST, table)
+    code_gen = llvmgen.generate_llvm(tac)
+    Compiller.run(code_gen)
+
